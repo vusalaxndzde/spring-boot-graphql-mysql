17f0faba7a81ac46ebc736a655585e96d9b69f19
vusalaxndzde <vusal.axndzde@gmail.com>
2023-08-31 21:05:40 +0400

    graphql example

--- src/main/java/com/vusalaxndzde/springbootgraphqlmysql/controller/AuthorController.java
+++ src/main/java/com/vusalaxndzde/springbootgraphqlmysql/controller/AuthorController.java
@@ -4,6 +4,7 @@ import com.vusalaxndzde.springbootgraphqlmysql.entity.Author;
 import com.vusalaxndzde.springbootgraphqlmysql.service.AuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -28,6 +29,21 @@ public class AuthorController {
     @QueryMapping
     public int countAuthors() {
         return authorService.countAuthors();
+    }
+
+    @MutationMapping
+    public Author createAuthor(@Argument String firstName, @Argument String lastName) {
+        return authorService.createAuthor(firstName, lastName);
+    }
+
+    @MutationMapping
+    public Author updateAuthor(@Argument long authorId, @Argument String firstName, @Argument String lastName) {
+        return authorService.updateAuthor(authorId, firstName, lastName);
+    }
+
+    @MutationMapping
+    public boolean deleteAuthor(@Argument long authorId) {
+        return authorService.deleteAuthor(authorId);
     }
 
 }
--- src/main/java/com/vusalaxndzde/springbootgraphqlmysql/service/AuthorService.java
+++ src/main/java/com/vusalaxndzde/springbootgraphqlmysql/service/AuthorService.java
@@ -2,6 +2,7 @@ package com.vusalaxndzde.springbootgraphqlmysql.service;
 
 import com.vusalaxndzde.springbootgraphqlmysql.entity.Author;
 import com.vusalaxndzde.springbootgraphqlmysql.repository.AuthorRepository;
+import com.vusalaxndzde.springbootgraphqlmysql.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.List;
 public class AuthorService {
 
     private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
 
     public List<Author> findAllAuthors() {
         return authorRepository.findAll();
@@ -23,6 +25,34 @@ public class AuthorService {
 
     public int countAuthors() {
         return authorRepository.findAll().size();
+    }
+
+    public Author createAuthor(String firstName, String lastName) {
+        Author author = Author.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .build();
+
+        return authorRepository.save(author);
+    }
+
+    public Author updateAuthor(long authorId, String firstName, String lastName) {
+        Author author = authorRepository.findById(authorId).orElseThrow();
+        author.setFirstName(firstName);
+        author.setLastName(lastName);
+
+        return authorRepository.save(author);
+    }
+
+    public boolean deleteAuthor(long authorId) {
+        Author author = authorRepository.findById(authorId).orElseThrow();
+        author.getBooks().forEach(book -> {
+                            book.setAuthor(null);
+                            bookRepository.save(book);
+                        });
+        authorRepository.delete(author);
+
+        return true;
     }
 
 }
