06102588f0e6d0a28144940bdde0e8c188981000
vusalaxndzde <vusal.axndzde@gmail.com>
2023-09-01 00:16:51 +0400

    graphql example

--- src/main/java/com/vusalaxndzde/springbootgraphqlmysql/SpringBootGraphqlMysqlApplication.java
+++ src/main/java/com/vusalaxndzde/springbootgraphqlmysql/SpringBootGraphqlMysqlApplication.java
@@ -4,8 +4,6 @@ import com.vusalaxndzde.springbootgraphqlmysql.entity.Author;
 import com.vusalaxndzde.springbootgraphqlmysql.entity.Book;
 import com.vusalaxndzde.springbootgraphqlmysql.repository.AuthorRepository;
 import com.vusalaxndzde.springbootgraphqlmysql.repository.BookRepository;
-import com.vusalaxndzde.springbootgraphqlmysql.service.AuthorService;
-import com.vusalaxndzde.springbootgraphqlmysql.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -23,24 +21,24 @@ public class SpringBootGraphqlMysqlApplication {
 	public final BookRepository bookRepository;
 	public final AuthorRepository authorRepository;
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner() {
-//		return args -> {
-//			Author author = Author.builder()
-//					.firstName("Albert")
-//					.lastName("Camus")
-//					.build();
-//
-//			authorRepository.save(author);
-//
-//			Book book = Book.builder()
-//					.name("The Stranger")
-//					.pageCount(120)
-//					.author(author)
-//					.build();
-//
-//			bookRepository.save(book);
-//		};
-//	}
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			Author author = Author.builder()
+					.firstName("Albert")
+					.lastName("Camus")
+					.build();
+
+			authorRepository.save(author);
+
+			Book book = Book.builder()
+					.name("The Stranger")
+					.pageCount(120)
+					.author(author)
+					.build();
+
+			bookRepository.save(book);
+		};
+	}
 
 }
